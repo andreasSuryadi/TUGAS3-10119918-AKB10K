51bd1d9029c46e5a80d35a40f2fad11ae05c2277
Andreas Suryadi <andreas.suryadi02@gmail.com>
2022-04-30 21:37:56 +0700

    First Commit

---
+++ app/src/main/java/com/example/a10119918latihan3/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.a10119918latihan3;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+import static com.example.a10119918latihan3.DoneActivity.ID_EXTRA_MSG_EXIT;
+
+
+/**
+ *
+ * NIM                  : 10119918
+ * Nama                 : Andreas Suryadi
+ * Kelas                : IF-10K
+ * Tanggal Pengerjaan   : 30 April 2022
+ *
+ */
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE); //will hide the title
+        getSupportActionBar().hide(); // hide the title bar
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN); //enable full screen
+        setContentView(R.layout.activity_welcome);
+
+        ButterKnife.bind(this);
+
+        if (getIntent().getBooleanExtra(ID_EXTRA_MSG_EXIT, false)) {
+            finish();
+        }
+    }
+
+    @OnClick(R.id.btnWalkthroughStart)
+    void mulai() {
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+    }
+}
